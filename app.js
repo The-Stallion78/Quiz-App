e2dd2adcd66a6c586415b8181f8c7999fc460625
The-Stallion78 <khizaerhayat@gmail.com>
2024-07-24 01:12:56 +0500

    ...

---
+++ app.js
@@ -0,0 +1,91 @@
+const questions = [
+    {
+        question: "Which is the longest river in the world?",
+        answers: [
+            {text: "Amazon River", correct: false},
+            {text: "Nile River", correct: true},
+            {text: "Indus River", correct: false},
+            {text: "Mississippi River", correct: false}
+        ] 
+    },
+
+    {
+        question: "Which country has the largest population in the world?",
+        answers: [
+            {text: "China", correct: true},
+            {text: "Pakistan", correct: false},
+            {text: "India", correct: false},
+            {text: "Russia", correct: false}
+        ]
+    },
+
+    {
+        question: "Which city is known as the 'Eternal City'?",
+        answers: [
+            {text: "London", correct: false},
+            {text: "Haripur", correct: false},
+            {text: "Rome", correct: true},
+            {text: "Kolkata", correct: false}
+        ]
+    },
+
+    {
+        question: "What is the official currency of Japan?",
+        answers: [
+            {text: "Yuan", correct: false},
+            {text: "Yen", correct: true},
+            {text: "Dollar", correct: false},
+            {text: "Ruble", correct: false}
+        ]
+    },
+
+    {
+        question: "Which desert is the largest in the world by area?",
+        answers: [
+            {text: "Sahara Desert", correct: true},
+            {text: "Gobi Desert", correct: false},
+            {text: "Albanian Desert", correct: false},
+            {text: "Kalahari Desert", correct: false}
+        ]
+    },
+
+    {
+        question: "Who was the first person to reach the South Pole?",
+        answers: [
+            {text: "oald Amundsen", correct: true},
+            {text: "Robert Falcon Scott", correct: false},
+            {text: " Ernest Shackleton", correct: false},
+            {text: "Edmund Hillary", correct: false}
+        ]
+    },
+
+    {
+        question: "Which is the smallest country in the world by land area?",
+        answers: [
+            {text: "Sri lanka", correct: false},
+            {text: "Cook Islands", correct: false},
+            {text: "Vatican City", correct: true},
+            {text: "Malta", correct: false}
+        ]
+    },
+
+    {
+        question: "",
+        answers: [
+            {text: "", correct: true},
+            {text: "", correct: false},
+            {text: "", correct: false},
+            {text: "", correct: false}
+        ]
+    },
+
+    {
+        question: "",
+        answers: [
+            {text: "", correct: true},
+            {text: "", correct: false},
+            {text: "", correct: false},
+            {text: "", correct: false}
+        ]
+    }
+]
